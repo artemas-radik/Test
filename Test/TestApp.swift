114c2fc4669fc33b5ae2a744be63faef2aaf5264
Artemas Radik <ajradik@gmail.com>
2022-11-29 03:04:35 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Artemas on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
